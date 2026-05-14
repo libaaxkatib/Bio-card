124b34009bb444af4fa62d690f621c615aae3051
Asad Mohamed <libaaxkatib@gmail.com>
2026-05-14 09:09:28 +0300

    first"

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const toggleButton = document.getElementById("theme-toggle");
 const body = document.body;
 const STORAGE_KEY = "preferred-theme";
-function handleToggleClick () {
-const isDarkNow = body.classList.toggle("dark");
-if (isDarkNow) {
-    toggleButton.textContent = "* Light mode";
-    localStorage.setItem(STORAGE_KEY, "dark");
-    }else{
+function handleToggleClick() {
+    const isDarkNow = body.classList.toggle("dark");
+    if (isDarkNow) {
+        toggleButton.textContent = " 🌑Light mode";
+        localStorage.setItem(STORAGE_KEY, "dark");
+    } else {
         toggleButton.textContent = "🌙 Dark mode";
         localStorage.setItem(STORAGE_KEY, "light");
     }
@@ -15,13 +15,13 @@ if (isDarkNow) {
 toggleButton.addEventListener("click", handleToggleClick);
 function loadSavedTheme() {
     const savedTheme = localStorage.getItem(STORAGE_KEY);
-    
+
     if (savedTheme === "dark") {
         body.classList.add("dark");
-        toggleButton.textContent = "* Light mode";
-         } else {
-            toggleButton.textContent = "🌙 Dark mode";
-         }
+        toggleButton.textContent = " 🌑Light mode";
+    } else {
+        toggleButton.textContent = "🌙 Dark mode";
+    }
 }
 
 loadSavedTheme();
